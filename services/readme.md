b7f355e5407ceadb31131c287c742a335a99537a
Marcin Polak <mapoart@gmail.com>
2020-08-26 11:50:51 +0200

    Upgrade to the latest versions for API/SERVICE. (also nodejs 14)

--- services/readme.md
+++ services/readme.md
@@ -1,26 +1,29 @@
-## Services
-# User Service
+# API
 
 All doc for now is in the in global file readme.md
 
 ## Install & run
+
 npm install && npm start
 
-## Some Security infos
-# Setup your SSH Keys
+## Setup your SSH Keys
+
 $ mkdir -p var/jwt
 $ openssl genrsa -out var/jwt/private.pem -aes256 4096
-$ openssl rsa -pubout -in var/jwt/private.pem -out var/jwt/public.pem
+\$ openssl rsa -pubout -in var/jwt/private.pem -out var/jwt/public.pem
 
-# Express && nginx && pm2
+## Express && nginx && pm2
+
 http://blog.danyll.com/setting-up-express-with-nginx-and-pm2/
 https://expressjs.com/en/advanced/best-practice-security.html
 https://www.npmjs.com/package/express-limiter
 
-# Nginx TLS Configuration
+## Nginx TLS Configuration
+
 https://mozilla.github.io/server-side-tls/ssl-config-generator/
 
-# Brute Force Attacks
+## Brute Force Attacks
+
 Nginx handle this. Configuration allow for it
 https://easyengine.io/tutorials/nginx/block-wp-login-php-bruteforce-attack/
 
--- services/src/server/db.js
+++ services/src/server/db.js
@@ -1,21 +1,35 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema } from "mongoose";
 
-const env = process.env.NODE_ENV || 'development'
-const config = require('./config')[env]
+const env = process.env.NODE_ENV || "development";
+const config = require("./config")[env];
 
-console.log(`Database name: ${config.database.name}`)
+console.log(`Database name: ${config.database.name}`);
 // Set up default mongoose connection
-var mongoDB = `mongodb://${config.database.host}/${config.database.name}`
-mongoose.Promise = global.Promise
-mongoose.connect(mongoDB, {useMongoClient: true})
-if (env === 'development') {
-  mongoose.set('debug', true)
+var mongoDB = `mongodb://${config.database.host}/${config.database.name}`;
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  // .then(() => console.log("DB Connected!"))
+  .catch((err) => {
+    console.log(`DB Connection Error: ${err.message}`);
+  });
+
+if (env === "development") {
+  mongoose.set("debug", true);
 }
 
 // Get the default connection
-var connection = mongoose.connection
+var connection = mongoose.connection;
 
 // Bind connection to error event (to get notification of connection errors)
-connection.on('error', console.error.bind(console, 'MongoDB connection error:'))
+connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
 
-export {mongoose, connection, Schema}
+export { mongoose, connection, Schema };
